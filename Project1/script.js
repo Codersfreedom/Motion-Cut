48a68b4dd1b21999a57c53da79f6d0b15e131ab1
BelugaBoy <rakeshmanna762@gmail,com>
2024-01-19 19:58:39 +0530

    project1 day5

--- Project1/script.js
+++ Project1/script.js
@@ -1,6 +1,10 @@
 const toggleThemebtn = document.querySelector("input[name=themeBtn]");
 const monthToggle = document.querySelector("input[name=month-year]");
 const select = document.getElementById("currency");
+const hamburger = document.querySelector('.hamburger-container')
+const togglelist = document.querySelector('.options')
+const menu = document.querySelector(".hamburger")
+const cross = document.querySelector('.cross');
 const newElement = document.createDocumentFragment();
 const standard = document.querySelector(".st-price");
 const ultimate = document.querySelector(".ul-price");
@@ -8,13 +12,22 @@ const free = document.querySelector(".price");
 var stPrice = 99;
 var prePrice = 149;
 var currency ={};
+var toggle = false;
+
 toggleThemebtn.addEventListener("change", function () {
   if (this.checked) {
     document.body.style.backgroundColor = "#192734";
     document.body.style.color = "white";
+    togglelist.style.backgroundColor ="#192734"; 
+    togglelist.style.color = 'white'
   } else {
     document.body.style.backgroundColor = "white";
     document.body.style.removeProperty("color");
+    togglelist.style.backgroundColor="white"
+    togglelist.style.color = 'black'
+
+
+
   }
 });
 
@@ -39,10 +52,11 @@ toggleThemebtn.addEventListener("change", function () {
   
 select.addEventListener("change", (e) => {
   if(e.target.value==='INR'){
+    monthToggle.checked = false;
     free.innerHTML = `&#8377 0`
     standard.innerHTML = `<p class="ul-price">&#8377 ${Math.ceil(stPrice*82.93809997)} <small>/ month </small>  </p>`;
     ultimate.innerHTML = `<p class="ul-price">&#8377 ${Math.ceil(prePrice*82.93809997)} <small>/ month </small>  </p>`;
-
+    
     monthToggle.addEventListener("change", function () {
       if (this.checked) {
         standard.innerHTML = `<p class="ul-price">&#8377 ${Math.ceil(stPrice*5.04*82.93809997)} <small>/ year </small>  </p>`;
@@ -55,6 +69,7 @@ select.addEventListener("change", (e) => {
     })
 
   }else if(e.target.value === 'USD'){
+    monthToggle.checked = false;
     free.innerHTML = `&#36 0`
     standard.innerHTML = `<p class="ul-price">&#36 ${Math.ceil(stPrice)} <small>/ month </small>  </p>`;
     ultimate.innerHTML = `<p class="ul-price">&#36 ${Math.ceil(prePrice)} <small>/ month </small>  </p>`;
@@ -81,3 +96,20 @@ monthToggle.addEventListener("change", function () {
     ultimate.innerHTML = `<p class="ul-price">&#36 ${prePrice} <small>/ month </small>  </p>`;
   }
 });
+
+hamburger.addEventListener('click',()=>{
+    // togglelist.removeProperty('class')
+    if(!toggle){
+      togglelist.classList.replace('options','toggle')
+      cross.style.display ='block';
+      menu.style.display = 'none';
+      toggle = true;
+
+    }else{
+      togglelist.classList.replace('toggle','options');
+      menu.style.display = 'block';
+      cross.style.display= 'none'
+      toggle = false;
+    }
+
+})
